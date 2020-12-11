8ff14f400c7389d09a003638e47798c116a0735a
leekilhun <pba7189@gmail.com>
2020-12-11 23:24:55 +0900

    테스트 코드

---
+++ IEnumerableTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+
+namespace IEnumerableTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] authors = { "A", "B", "C", "D", "E", "F" };
+            IEnumerator e = authors.GetEnumerator();
+
+            while (e.MoveNext())
+            {
+                Console.WriteLine(e.Current);
+            }
+        }
+    }
+}
